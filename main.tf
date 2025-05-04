2da53fa98a07ae6d6321ee92382e6da5fefa059d
kasundradrashti <42646925+kasundradrashti@users.noreply.github.com>
2025-05-04 16:30:16 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,4 @@
-resource "google_storage_bucket" "auto-expire" {
-  name          = "cloud-build-demo-bucket"
-  location      = "US"
-  project       = "drashtik-ifs-dev"
-  force_destroy = true
 
-  public_access_prevention = "enforced"
-}
 
 resource "google_storage_bucket" "auto-expire2" {
   name          = "cloud-build-demo-bucket1"
